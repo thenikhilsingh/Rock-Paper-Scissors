999840ca18c43a080d83803a2e724a8e31b0d6bc
Nikhil Singh <nikhilsingh26042004@gmail.com>
2024-12-19 00:47:41 +0530

    modified index.html, style.css and script.js files

--- script.js
+++ script.js
@@ -1,40 +1,77 @@
 let handSign = ["rock", "paper", "scissors"];
-//let userInput = prompt();
 
 function randomComputerHandSigns() {
   let x = Math.floor(Math.random() * 3);
   return handSign[x];
 }
 
+let userInput;
+
+let gameResult=document.querySelector("#gameResult")
+
 function gameResultChecker() {
-if (userInput == "rock" && randomComputerHandSigns() == "handSign[0]") {
-  console.log("The match was Draw");
+  let computerInput=randomComputerHandSigns();
+
+if (userInput == "rock" && computerInput == handSign[0]) {
+gameResult.innerHTML="The match was Draw as the computer choose rock"
 } 
-else if (userInput == "rock" && randomComputerHandSigns() == "handSign[1]") {
-  console.log("You lost the match");
+else if (userInput == "rock" && computerInput == handSign[1]) {
+  gameResult.innerHTML="You lost the match as the computer choose paper"
 } 
-else if (userInput == "rock" && randomComputerHandSigns() == "handSign[2]") {
-  console.log("You won the match");
+else if (userInput == "rock" && computerInput == handSign[2]) {
+  gameResult.innerHTML="You won the match as the computer choose scissors"
 }
-else if (userInput == "paper" && randomComputerHandSigns() == "handSign[0]") {
-  console.log("You won the match");
+else if (userInput == "paper" && computerInput == handSign[0]) {
+  gameResult.innerHTML="You won the match as the computer choose rock"
 }
-else if (userInput == "paper" && randomComputerHandSigns() == "handSign[1]") {
-  console.log("The match was Draw");
+else if (userInput == "paper" && computerInput == handSign[1]) {
+  gameResult.innerHTML="The match was Draw as the computer choose paper"
 }
-else if (userInput == "paper" && randomComputerHandSigns() == "handSign[2]") {
-  console.log("You lost the match");
+else if (userInput == "paper" && computerInput == handSign[2]) {
+  gameResult.innerHTML="You lost the match as the computer choose scissors"
 }
-else if (userInput == "scissors" && randomComputerHandSigns() == "handSign[0]") {
-  console.log("You lost the match");
+else if (userInput == "scissors" && computerInput == handSign[0]) {
+  gameResult.innerHTML="You lost the match as the computer choose rock"
 }
-else if (userInput == "scissors" && randomComputerHandSigns() == "handSign[1]") {
-  console.log("You won the match");
+else if (userInput == "scissors" && computerInput == handSign[1]) {
+  gameResult.innerHTML="You won the match as the computer choose paper"
 }
-else if (userInput == "scissors" && randomComputerHandSigns() == "handSign[2]") {
-    console.log("The match was Draw");
+else if (userInput == "scissors" && computerInput == handSign[2]) {
+    gameResult.innerHTML="The match was Draw as the computer choose scissors"
 }
 else {
-  console.log("enter a valid input");
+  gameResult.innerHTML="enter a valid input"
 }
 }
+
+
+let rock=document.querySelector("#rock");
+rock.addEventListener("click",()=>{
+  userInput="rock"
+  gameResultChecker()
+})
+
+let paper=document.querySelector("#paper");
+paper.addEventListener("click",()=>{
+  userInput="paper"
+  gameResultChecker()
+})
+
+let scissors=document.querySelector("#scissors");
+scissors.addEventListener("click",()=>{
+  userInput="scissors"
+  gameResultChecker()
+})
+
+
+let yourScorePoints=document.querySelector("#yourScorePoints")
+let computerScorePoints=document.querySelector("#computerScorePoints")
+yourScorePoints.innerHTML=0;
+computerScorePoints.innerHTML=0;
+function userScore() {
+ for (let i = 0; i < 11; i++) {
+  return yourScorePoints.innerHTML+i;
+ }
+}
+
+
